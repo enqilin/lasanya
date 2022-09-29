50f436476c9edb7f964d16dd3e24280eaadbd703
nad0us <114181724+enqilin@users.noreply.github.com>
2022-09-29 14:26:39 +0000

    LASAÑA

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
-print("hi")
+EXPECTED_BAKE_TIME=40
+PREPARATION_TIME=2
+def bake_time_remaining(elapsed_bake_time):
+  return  EXPECTED_BAKE_TIME-elapsed_bake_time
+def preparation_bake_in_minutes(number_of_layers):
+  return number_of_layers * PREPARATION_TIME
+def elapsed_bake_in_minutes(number_of_layers,elapsed_bake_time):
+  return preparation_bake_in_minutes(number_of_layers) + elapsed_bake_time
